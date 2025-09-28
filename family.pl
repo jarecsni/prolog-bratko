3b3ec9e9c89b410e7e09edb08739485d5c2c2220
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2025-09-28 22:02:36 +0100

    🚧 Family again - expressing relationships

--- family.pl
+++ family.pl
@@ -5,5 +5,24 @@ parent(bob, ann).
 parent(bob, pat).
 parent(pat, jim).
 
-offspring(Parent, Child) :- parent(Parent, Child).
-offspring(Parent, Descendant) :- parent(Parent, Intermediate), offspring(Intermediate, Descendant).
+female(pam).            % alternative could be sex(pam, female)
+female(liz).
+female(pat).
+female(ann).
+male(jim).
+male(tom).
+male(bob).
+
+different(X, Y) :- X \= Y.
+
+offspring(Child, Parent) :- parent(Parent, Child).
+% offspring(Descendant, Parent) :- parent(Parent, Intermediate), offspring(Descendant, Intermediate).
+
+mother(Child, Mother) :- parent(Mother, Child), female(Mother).
+father(Child, Father) :- parent(Father, Child), male(Father).
+
+grandparent(Grandparent, Grandchild) :- parent(Grandparent, Parent), parent(Parent, Grandchild).
+grandfather(Grandchild, Grandfather) :- grandparent(Grandfather, Grandchild), male(Grandfather).
+grandmother(Grandchild, Grandmother) :- grandparent(Grandmother, Grandchild), female(Grandmother).
+
+sister(Sister, SisterOf) :- parent(Parent, Sister), parent(Parent, SisterOf), female(Sister), different(Sister, SisterOf).
