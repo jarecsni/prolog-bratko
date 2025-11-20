2ef9cd6d8df6a114f52f92595299d29762df0300
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2025-11-20 06:57:20 +0000

    ✨ exercise 3.13

--- 3_3_operators.pl
+++ 3_3_operators.pl
@@ -1,3 +1,4 @@
+% 3.12 
 :- op(300, xfx, plays).
 :- op(200, xfy, and).
 
@@ -10,3 +11,14 @@ test2 :-
     Term = (susan plays tennis and basketball and volleyball),
     write('Pretty: '), write(Term), nl,
     write('Canonical: '), write_canonical(Term), nl.
+
+% 3.13
+:- op(500, xfx, was).
+:- op(400, xfx, of).
+:- op(300, fx, the).
+diana was the secretary of the department.
+test3 :- 
+    Term = (diana was the secretary of the department),
+    write('Pretty: '), write(Term), nl,
+    write('Canonical: '), write_canonical(Term), nl.
+
